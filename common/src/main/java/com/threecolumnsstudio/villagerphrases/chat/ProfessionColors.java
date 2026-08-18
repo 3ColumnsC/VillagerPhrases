747cdefa0a065c8cd44ae17f89ac5279f68b8986
3ColumnsC <3columnscreations@protonmail.com>
2026-08-18 19:19:02 +0200

    Minor change

--- common/src/main/java/com/threecolumnsstudio/villagerphrases/chat/ProfessionColors.java
+++ common/src/main/java/com/threecolumnsstudio/villagerphrases/chat/ProfessionColors.java
@@ -12,19 +12,19 @@ public final class ProfessionColors {
     public static final int NO_COLOR = -1;
 
     private static final Map<String, Integer> COLORS = Map.ofEntries(
-        Map.entry("farmer",        0xFF8B5A2B),
-        Map.entry("fisherman",     0xFF2E6F95),
-        Map.entry("shepherd",      0xFF6B4423),
-        Map.entry("fletcher",      0xFF8B7355),
+        Map.entry("farmer",        0xFFC89B5A),
+        Map.entry("fisherman",     0xFF5CA6CC),
+        Map.entry("shepherd",      0xFFB8976A),
+        Map.entry("fletcher",      0xFFC7A88A),
         Map.entry("librarian",     0xFFEDE4D3),
         Map.entry("cartographer",  0xFFB8D8E8),
-        Map.entry("cleric",        0xFF7B4FA1),
-        Map.entry("armorer",       0xFF4A4A4A),
-        Map.entry("weaponsmith",   0xFF5C4033),
-        Map.entry("toolsmith",     0xFF7A6B5A),
-        Map.entry("butcher",       0xFFA8312A),
-        Map.entry("leatherworker", 0xFF8B4513),
-        Map.entry("mason",         0xFFA89B8C)
+        Map.entry("cleric",        0xFF9B6FC4),
+        Map.entry("armorer",       0xFF9A9A9A),
+        Map.entry("weaponsmith",   0xFF9C7A60),
+        Map.entry("toolsmith",     0xFFA99985),
+        Map.entry("butcher",       0xFFD96B62),
+        Map.entry("leatherworker", 0xFFC98A54),
+        Map.entry("mason",         0xFFBCB0A0)
     );
 
     private ProfessionColors() {}
